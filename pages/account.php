2b2d140c71a2cdaeb9146a2e72c295d74d8cf3b4
kekszharcos <b.enedek670@gmail.com>
2023-04-06 19:39:07 +0200

    log-ready

--- pages/account.php
+++ pages/account.php
@@ -13,21 +13,60 @@ foreach ($users["users"] as $user){
 <main>
     <section class="article-imitator">
         <div class="flex-box">
-
             <?php
-            if (isset($_SESSION["username"]) && !$admin){
+            if (isset($_SESSION["username"]) && $admin){
                 ?>
-                <div class="card">Settings</div>
-
-                <?php
-            }else{
-                ?>
-                <div class="card">Settings</div>
-                <div class="card">Manage users</div>
+                <div class="card">
+                    <a>Manage users</a>
+                </div>
                 <?php
             }
-            ?>
-
+                ?>
+            <div class="card">
+                <a>Settings</a>
+            </div>
+            <div class="card">
+                <a>Niffer</a>
+            </div>
+            <div class="card">
+                <a>Buffer</a>
+            </div>
+            <div class="card">
+                <a>Filler</a>
+            </div>
+            <div class="card">
+                <a>Tiller</a>
+            </div>
+            <div class="card">
+                <a>Siller</a>
+            </div>
+            <div class="card">
+                <a>Kill</a>
+            </div>
+            <div class="card">
+                <a>Breer</a>
+            </div>
+            <div class="card">
+                <a>Fleer</a>
+            </div>
+            <div class="card">
+                <a>Beer</a>
+            </div>
+            <div class="card">
+                <a>Fire Water</a>
+            </div>
+            <div class="card">
+                <a>Saxof</a>
+            </div>
+            <div class="card">
+                <a>Construction</a>
+            </div>
+            <div class="card">
+                <a>Klomel</a>
+            </div>
+            <div class="card">
+                <a>Felis</a>
+            </div>
         </div>
     </section>
 </main>
