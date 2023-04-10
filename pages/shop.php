acf80d199d9b9185dcb799c8b3cc637b7914c943
Medi <csetemedard@gmail.com>
2023-04-10 19:00:21 +0200

    .

--- pages/shop.php
+++ pages/shop.php
@@ -14,26 +14,32 @@
             audio.volume = 0.06;
         </script>
 
-
+        <div class="overlay" id="overlay">
         <div class="popup" id="popup">
             <h2>Please enter the following information</h2>
             <form method="post" id="form_login" enctype="multipart/form-data" autocomplete="on">
                 <fieldset>
+                <label for="payment">Payment</label>
                 <label for="card-number">Card Number:</label>
                 <input type="text" id="card-number" name="card-number">
                 <label for="expiry-date">Expiry Date:</label>
                 <input type="text" id="expiry-date" name="expiry-date">
                 <label for="cvv">CVV:</label>
                 <input type="text" id="cvv" name="cvv">
-                <label for="name">Name:</label>
+                <label for="name">Cardholer Name:</label>
                 <input type="text" id="name" name="name">
-                <label for="address">Address:</label>
-                <textarea id="address" name="address"></textarea>
+                <label for="address">Address</label>
+                    <label for="address">Zip code:</label>
+                    <input type="text" id="zip" name="zip">
+                    <label for="address">City:</label>
+                    <input type="text" id="city" name="city">
+                    <label for="address">Street and house number:</label>
+                    <input type="text" id="street" name="street">
                 </fieldset>
             </form>
             <button onclick="closePopup()">Proceed</button>
         </div>
-
+        </div>
 
         <div class="shop-item">
             <img src="../sources/shop/blackshirt.png" alt="blackshirt"><br>
@@ -77,13 +83,15 @@
         </div>
            <script>
                let popup = document.getElementById("popup");
+               let overlay = document.getElementById("overlay")
 
                function openPopup(){
                     popup.classList.add("open-popup");
-
+                    overlay.classList.add("overlay-open");
                }
                function closePopup(){
                    popup.classList.remove("open-popup");
+                   overlay.classList.remove("overlay-open");
                }
            </script>
 
